c75a7be31e5e49eeddcebc8dd641c2ec1c6ad95b
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-24 23:07:25 +0530

    add

--- ecommerce_project/flipkart/admin.py
+++ ecommerce_project/flipkart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# from .models import CustomUser, Category, Product
 
-# Register your models here.
+# # Register your models here.
+# admin.register(CustomUser)
+# admin.register(Category)
+# admin.register(Product)
