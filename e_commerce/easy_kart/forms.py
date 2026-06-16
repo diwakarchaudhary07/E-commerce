f8d61ed9f263e4869943ddfd902489f1c0f24811
diwakar chaudhary <boyroyal4853@gmail.com>
2026-06-16 21:48:59 +0530

    add

---
+++ e_commerce/easy_kart/forms.py
@@ -0,0 +1,49 @@
+﻿from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+
+from .models import CustomUser
+
+
+class RegisterForm(forms.Form):
+    full_name = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
+    confirm_password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label='Confirm Password',
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise ValidationError('Email is already registered.')
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError('Passwords do not match.')
+
+        if password:
+            validate_password(password)
+
+        return cleaned_data
+
+
+class OTPVerificationForm(forms.Form):
+    otp_code = forms.CharField(
+        max_length=6,
+        min_length=6,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+        label='OTP Code',
+    )
+
