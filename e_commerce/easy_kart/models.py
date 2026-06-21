5f27f2ff91e95785a943c17da2e8aa8943da476e
diwakar chaudhary <boyroyal4853@gmail.com>
2026-06-21 17:35:45 +0530

    add

--- e_commerce/easy_kart/models.py
+++ e_commerce/easy_kart/models.py
@@ -97,6 +97,7 @@ class Category(models.Model):
 	name = models.CharField(max_length=200, unique=True)
 	slug = models.SlugField(max_length=200, unique=True)
 	description = models.TextField(blank=True, null=True)
+	image = models.ImageField(upload_to="categories/", blank=True, null=True)
 	is_active = models.BooleanField(default=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
