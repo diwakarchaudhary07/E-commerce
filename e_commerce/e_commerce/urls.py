10f457e8029fa230abd92589280af8538568567e
diwakar chaudhary <boyroyal4853@gmail.com>
2026-06-28 22:19:46 +0530

    add

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('easy_kart.urls')),
+    # Include Django auth views (login/logout/password change/reset)
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
