06a153e6a839a68a283189a66653cccfce2a6904
diwakar chaudhary <boyroyal4853@gmail.com>
2026-08-06 23:37:08 +0530

    add

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -18,10 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from easy_kart.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('easy_kart.urls')),
+    path('accounts/password_reset/', password_reset, name='password_reset'),
+    path('accounts/password_reset/done/', password_reset_done, name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', password_reset_confirm, name='password_reset_confirm'),
+    path('accounts/reset/done/', password_reset_complete, name='password_reset_complete'),
     # Include Django auth views (login/logout/password change/reset)
     path('accounts/', include('django.contrib.auth.urls')),
 ]
